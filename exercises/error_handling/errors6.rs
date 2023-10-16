616e5d1568152a185e2669a1cd15d83068e7462e
liyang <96541133@163.com>
2023-10-16 16:28:47 +0800

    liyang_rust_text

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -9,8 +9,6 @@
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
@@ -24,17 +22,25 @@ impl ParsePosNonzeroError {
     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
-    // TODO: add another error conversion function here.
-    // fn from_parseint...
+
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
-    // TODO: change this to return an appropriate error instead of panicking
-    // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
-    PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
-}
+    let x: Result<i64, ParseIntError> = s.parse();
 
+    match x {
+        Ok(value) => {
+            match PositiveNonzeroInteger::new(value) {
+                Ok(result) => Ok(result),
+                Err(creation_err) => Err(ParsePosNonzeroError::from_creation(creation_err)),
+            }
+        }
+        Err(parse_err) => Err(ParsePosNonzeroError::from_parseint(parse_err)),
+    }
+}
 // Don't change anything below this line.
 
 #[derive(PartialEq, Debug)]
